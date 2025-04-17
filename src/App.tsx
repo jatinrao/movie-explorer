45a548c5cd906c4babe30727c1d6f965e419acdf
Jatin Rao <jatinrao.jr15@gmail.com>
2025-04-17 17:39:11 +0530

    Initial Environment Setup + husky pre commit hook test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const App = () => {
   return (
     <div>
-           <h1>Movie Explorer</h1>
+      {/* husky pre commit test */}
+      <h1>Movie Explorer</h1>
       <p>Welcome to the Movie Explorer app!</p>
     </div>
   );
